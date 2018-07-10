d7ee9fcc0d6d198394cfa35c2fa531719fd0fbf1
Victor Carpenter <victorcarpenter123@gmail.com>
2018-07-10 10:38:39 -0400

    trying seperation

--- wp-content/themes/Enviro/functions.php
+++ wp-content/themes/Enviro/functions.php
@@ -165,20 +165,3 @@ function enviro_register_fields() {
 	);
 }
 add_action( 'rest_api_init', 'enviro_register_fields' );
-function enviro_get_author_name( $object, $field_name, $request ) {
-	return get_the_author_meta( 'display_name' );
-}
-function enviro_get_image_src( $object, $field_name, $request ) {
-    if($object[ 'featured_media' ] == 0) {
-        return $object[ 'featured_media' ];
-    }
-	$feat_img_array = wp_get_attachment_image_src( $object[ 'featured_media' ], 'thumbnail', true );
-    return $feat_img_array[0];
-}
-function enviro_published_date( $object, $field_name, $request ) {
-	return get_the_time('F j, Y');
-}
-function enviro_excerpt_length( $length ) {
-    return 20;
-}
-add_filter( 'excerpt_length', 'enviro_excerpt_length' );
