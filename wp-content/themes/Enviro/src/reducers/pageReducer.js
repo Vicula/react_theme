8c22982169e802f08b61705fbc7ffd72b7ceaa2a
Victor Carpenter <victorcarpenter123@gmail.com>
2018-07-11 14:13:26 -0400

    got rid of switch

--- wp-content/themes/Enviro/src/reducers/pageReducer.js
+++ wp-content/themes/Enviro/src/reducers/pageReducer.js
@@ -3,21 +3,5 @@ import {FETCH_STUFF, RECEIVE_STUFF} from '../actions/actionTypes';
 
 
 export default function page(state = initialState.page, action) {
-  let newState;
-  switch (action.type) {
-    case FETCH_STUFF:
-      console.log('FETCH_STUFF Action')
-      return action;
-    case RECEIVE_STUFF:
-      newState = action.stuff;
-      console.log('RECEIVE_STUFF Action')
-      return newState;
-    default:
-      if(!state){
-        return initialState.page
-      }else{
-        return state;
-      }
-
-  }
+  return state;
 }
