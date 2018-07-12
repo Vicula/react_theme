2fe3c0f81a1b6dac09bcf0a28dbe8b04a60ef9b0
Victor Carpenter <victorcarpenter123@gmail.com>
2018-07-12 10:58:04 -0400

    starting on about page

---
+++ wp-content/themes/Enviro/src/views/about/about.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import LoadingIcon from '../../../dist/images/loading-icon.gif';
+import HomeHeader from './home-header';
+import LongCTA from './home-longCTA';
+import DuoCTA from './home-duoCTA';
+import BlogCTA from './home-blogCTA';
+
+class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+
+
+    componentDidMount() {
+        var that = this;
+        window.onbeforeunload = function () { window.scrollTo(0, 0); }
+        jQuery('.loading-panel').addClass('hide');
+    }
+
+    componentDidUpdate() {
+
+    }
+
+    render() {
+        return (
+
+                <div className="container homePage">
+                  <HomeHeader cf={this.props.pageDets.custom_fields}/>
+                  <LongCTA cf={this.props.pageDets.custom_fields}/>
+                  <DuoCTA cf={this.props.pageDets.custom_fields}/>
+                  <BlogCTA cf={this.props.pageDets.custom_fields}/>
+                  <DuoCTA cf={this.props.pageDets.custom_fields}/>
+                  <LongCTA cf={this.props.pageDets.custom_fields}/>
+                  <div className="parallaxImg"><img src="http://newcobblehill.wpengine.com/wp-content/uploads/2018/07/Untitled-2.jpg" /></div>
+                </div>
+
+        );
+    }
+}
+
+export default Home;
